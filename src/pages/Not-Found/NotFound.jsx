f94cbc3f5ff3e19334b0f99b80d66c27bf8334bd
siammahamud <siammahamud710@gmail.com>
2024-12-14 22:38:45 +0600

    [DONE] not-found page and route added

--- src/pages/Not-Found/NotFound.jsx
+++ src/pages/Not-Found/NotFound.jsx
@@ -1,9 +1,30 @@
-
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center p-12 bg-transparent rounded-lg shadow-2xl animate-fade-in-up shadow-orange-400">
+        <h1 className="text-9xl font-bold text-red-400 mb-4 animate-bounce">
+          404
+        </h1>
+        <p className="text-xl text-gray-800 mb-6 animate-pulse">
+          Oops! The page you are looking for does not exist.
+        </p>
+        <button
+          className="px-6 py-3 font-semibold text-white bg-cyan-600 rounded-md hover:bg-blue-600 transition-colors duration-200"
+          onClick={handleGoHome}
+        >
+          Go Back Home
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
